43c92c1193ba3b2f425b226aafde819d425633e9
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-10-20 02:49:55 -0800

    version10

--- Divhello.java
+++ Divhello.java
@@ -6,7 +6,7 @@ int a=20,b=40,c;
 c=a+b;
 System.out.println ("The first number is :"+a);
 System.out.println ("The second number is :"+b);
-System.out.println ("The result ankur gupta addition of two number is :"    +c);
+System.out.println ("The result ankur gupta is a good boy addition of two number is :"    +c);
 
 }
 
