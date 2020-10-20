d0b387745a8949aaa10106b62d4749e9f8223952
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-10-20 00:08:45 -0800

    version2

--- Divhello.java
+++ Divhello.java
@@ -6,7 +6,7 @@ int a=200,b=100,c;
 c=a/b;
 System.out.println ("The first number is :"+a);
 System.out.println ("The second number is :"+b);
-System.out.println ("The result DIVISION of two number is :"+c);
+System.out.println ("The result ankur DIVISION of two number is :"    +c);
 
 }
 
