c0d1013e0331956552bbb564ab00f6281bd47ab1
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-10-20 02:35:20 -0800

    version5

--- Divhello.java
+++ Divhello.java
@@ -2,11 +2,11 @@ class Divhello
 {
 public static void main (String args[])
 {
-int a=200,b=100,c;
-c=a-b;
+int a=20,b=40,c;
+c=a*b;
 System.out.println ("The first number is :"+a);
 System.out.println ("The second number is :"+b);
-System.out.println ("The result ankur subtraction of two number is :"    +c);
+System.out.println ("The result ankur gupta multiplication of two number is :"    +c);
 
 }
 
