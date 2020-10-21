d605d4ca47006bb3583f8a4e62bc4d03ab1dfa50
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-10-21 02:01:12 -0800

    version14

--- Divhello.java
+++ Divhello.java
@@ -3,7 +3,7 @@ class Divhello
 public static void main (String args[])
 {
 int a=20,b=40,c;
-c=a.b;
+c=a*b
 System.out.println ("The first number is :"+a);
 System.out.println ("The second number is :"+b);
 System.out.println ("The result ankita gupta is a good boy multiplication of two number is :"    +c);
